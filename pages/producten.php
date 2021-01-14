ff76aa6b5da004838720ed30dddff32c7845e2d9
Mees Lindeman <mees.lindeman@student.uva.nl>
2021-01-14 16:10:58 +0100

    WOrking on product page

--- pages/producten.php
+++ pages/producten.php
@@ -4,16 +4,18 @@
         <div class="row products">
     <?php endif?>
             <article class="four columns product">
-                <img src="./images/<?php echo $row['id']?>.jpg" alt="<?php echo $row['name']?>" />
-                <div class="body">
-                    <h4><?php echo $row['name']?></h4>
-                    <p class="price">
-                        <span class="currency">€</span>
-                        <span class="integers"><?php echo intval($row['price']/100)?></span>
-                        <span class="decimals">,<?php echo str_pad(fmod($row['price'], 100), 2, '0')?></span>
-                    </p>
-                    <button type="button"><i class="fas fa-cart-plus"></i> In winkelwagen</button>
-                </div>
+                <a href="?page=product&amp;category=<?php echo @$_REQUEST['category']?>&amp;product=<?php echo $row['id']?>">
+                    <img src="./images/<?php echo $row['id']?>.jpg" alt="<?php echo $row['name']?>" />
+                    <div class="body">
+                        <h4><?php echo $row['name']?></h4>
+                        <p class="price">
+                            <span class="currency">€</span>
+                            <span class="integers"><?php echo intval($row['price']/100)?></span>
+                            <span class="decimals">,<?php echo str_pad(fmod($row['price'], 100), 2, '0')?></span>
+                        </p>
+                        <button type="button"><i class="fas fa-cart-plus"></i> In winkelwagen</button>
+                    </div>
+                </a>
             </article>
     <?php if ($i % 3 == 2):?></div><?php endif?>
 <?php endforeach?>
