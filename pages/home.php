652bdc60f363084d4912c2a3d025e81aef2cdf90
Mark Lindeman <m.lindeman@picturae.com>
2021-01-14 15:45:38 +0100

    Makes products in Card-like layout

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,10 @@
-<div class="row products teasers">
+<div class="row products categories">
 <?php foreach ($app->getTeasers() as $i => $row) : ?>
-    <article class="four columns product">
-        <h4><a href="?page=producten&amp;category=<?php echo $row['id']?>"><?php echo $row['name']?></a></h4>
-        <!-- <h5><?php echo $row['product']['name']?></h5> -->
-        <a href="?page=producten&amp;category=<?php echo $row['id']?>">
-            <img src="./images/<?php echo $row['product']['id']?>.jpg" alt="<?php echo $row['product']['name']?>" />
-        </a>
-    </article>
+        <article class="four columns product">
+            <a href="?page=producten&amp;category=<?php echo $row['id']?>">
+                <img src="./images/<?php echo $row['product']['id']?>.jpg" alt="<?php echo $row['product']['name']?>" />
+                <h4><?php echo $row['name']?></h4>
+            </a>
+        </article>
 <?php endforeach?>
 </div>
