1d93c302d6b17441fc0bb0baad07a78a8bcd4ba9
Mark Lindeman <m.lindeman@picturae.com>
2021-01-14 17:31:48 +0100

    changes and simplifies user and login forms

--- pages/inloggen.php
+++ pages/inloggen.php
@@ -1,14 +1,16 @@
 <form action="?inloggen" method="POST">
-    <p>
-      <input type="hidden" name="login" value="1">
-      <label for="email">Emailadres</label>
-      <input type="email" name="email" class="form-control" id="email" required>
-    </p>
-    <p>
-      <label for="password">Wachtwoord</label>
-      <input type="password" name="password" class="form-control" id="password">
-    </p>
-    <p>
-      <button type="submit" class="button-primary"><i class="fas fa-sign-in-alt"></i>  Inloggen</button>
-    </p>
+  <div class="row">
+      <div class="three columns">
+          <input type="hidden" name="login" value="1">
+          <label for="email">Emailadres</label>
+          <input type="email" name="email" id="email" required>
+      </div>
+      <div class="three columns">
+          <label for="password">Wachtwoord</label>
+          <input type="password" name="password" id="password">
+      </div>
+      <div class="three columns">
+        <label>&nbsp;</label>
+        <button type="submit" class="button-primary"><i class="fas fa-sign-in-alt"></i>  Inloggen</button>
+      </div>
 </form>
--- pages/profiel.php
+++ pages/profiel.php
@@ -8,56 +8,45 @@ if (isset($_POST['profiel'])) {
 <form class="profiel" action="?page=profiel" method="POST">
     <div class="row">
         <div class="four columns">
-            <p>
-                <input type="hidden" name="profiel" value="1">
-              <label for="naam">Naam <i class="fas fa-asterisk"></i></label>
-              <input type="text" name="name" class="form-control" id="name" required value="<?php echo $user['name']?>">
-            </p>
+            <input type="hidden" name="profiel" value="1">
+            <label for="naam">Naam <i class="fas fa-asterisk"></i></label>
+            <input type="text" name="name" id="name" required value="<?php echo $user['name']?>">
         </div>
         <div class="four columns">
-            <p>
-              <label for="email">Emailadres <i class="fas fa-asterisk"></i></label>
-              <input type="email" name="email" class="form-control" id="email" required value="<?php echo $user['email']?>">
-            </p>
+            <label for="email">Emailadres <i class="fas fa-asterisk"></i></label>
+            <input type="email" name="email" id="email" required value="<?php echo $user['email']?>">
         </div>
     </div>
     <p></p>
     <div class="row">
         <div class="two columns">
-            <p>
-                <label for="postalcode">Postcode</label>
-                <input type="text" name="postalcode" class="form-control" id="postalcode" value="<?php echo $user['postalcode']?>">
-            </p>
+            <label for="postalcode">Postcode</label>
+            <input type="text" name="postalcode" id="postalcode" value="<?php echo $user['postalcode']?>">
         </div>
         <div class="two columns">
-            <p>
-                <label for="housenumber">Huisnummer</label>
-                <input type="text" name="housenumber" class="form-control" id="housenumber" value="<?php echo $user['housenumber']?>">
-            </p>
+            <label for="housenumber">Huisnummer</label>
+            <input type="text" name="housenumber" id="housenumber" value="<?php echo $user['housenumber']?>">
         </div>
         <div class="three columns">
-            <p>
-              <label for="phone">Telefoon </label>
-              <input type="text" name="phone" class="form-control" id="phone" value="<?php echo $user['phone']?>">
-            </p>
+            <label for="phone">Telefoon </label>
+            <input type="text" name="phone" id="phone" value="<?php echo $user['phone']?>">
         </div>
     </div>
     <div class="row">
         <div class="three columns">
-            <p>
-                <label for="password">Wachtwoord</label>
-                <input type="password" name="password" class="form-control" id="password">
-            </p>
+            <label for="password">Wachtwoord</label>
+            <input type="password" name="password" id="password">
         </div>
         <div class="three columns">
-            <p>
-                <label for="password2"> (controle)</label>
-                <input type="password" name="password2" class="form-control" id="password2">
-            </p>
+            <label for="password2"> (controle)</label>
+            <input type="password" name="password2" id="password2">
         </div>
     </div>
 
-    <p>
-      <button type="submit" class="button-primary"><i class="far fa-save"></i>  Opslaan</button>
-    </p>
+    <div class="row">
+        <div class="three columns">
+            <label>&nbsp;</label>
+            <button type="submit" class="button-primary"><i class="far fa-save"></i>  Opslaan</button>
+        </div>
+    </div>
 </form>
