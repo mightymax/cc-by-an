affe50447c060b7ea1d6321371b5be2f7d0f19e7
Mees Lindeman <mees.lindeman@student.uva.nl>
2021-01-26 12:51:51 +0100

    Team page

--- pages/addproduct.php
+++ pages/addproduct.php
@@ -11,55 +11,6 @@ endif;
 if (isset($_REQUEST['product'])) {
     $product = $app->getProduct($_REQUEST['product']);
 }
-
-function uploadImg(Array $data) {
-    $target_dir = "webtech-webshop/images/products/small";
-    $target_file = $target_dir . basename($_FILES["productimg"]['name']);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-    // Check if image file is an actual image
-    if (isset($_POST['submit'])) {
-        $check = getimagesize($_FILES["productimg"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-          } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-          }
-    }
-    //Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, this file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["productimg"]["size"] > 100) { 
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-
-    //File dimensions must by 300x300
-
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "jpeg") {
-        echo "Sorry, only JPG & JPEG are allowed.";
-        $uploadOk = 0;
-    } 
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-    if (move_uploaded_file($_FILES["productimg"]["tmp_name"], $target_file)) {
-      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    } else {
-      echo "Sorry, there was an error uploading your file.";
-    }
-  }  
-}
-
 ?>
 
 <br>
@@ -72,8 +23,8 @@ function uploadImg(Array $data) {
             <input type="text" name="name" id="name" value="<?php echo @$product['name']?>">
         </div>
         <div class="four columns">
-            <label for="productImg">Upload foto</label>
-            <input type="file" id="productimg" name="productimg">
+            <label for="image">Upload foto</label>
+            <input type="file" id="image" name="image">
         </div>
     </div>
     <div class="row">
--- pages/team.php
+++ pages/team.php
@@ -5,24 +5,23 @@
 <article>
     <div class="row">
         <div class="three columns">
-            <img src="./images/team/mees.jpg" alt="Mees Lindeman" width="200px" height="200px">
+            <img class="team" src="./images/team/mees.jpg" alt="Mees Lindeman" width="200" height="200">
         </div>
         <div class="seven columns">
             <h5>Mees Lindeman</h5>
-            <p>Hallo! Mijn naam is Mees en ik woon in Amsterdam. 
-            Voor mijn studie KI moest ik een website bouwen en het eerste wat in mij opkwam was een webshop voor m'n moeder.
+            <p>Hallo! Mijn naam is Mees en ik studeer in Amsterdam aan de UvA. 
+            Voor mijn studie KI kreeg ik de opdracht om een website bouwen en het eerste wat in mij opkwam was een webshop voor m'n moeder.
             Ik wilde haar graag helpen bij het toegankelijker maken van haar producten.
-            Met behulp van mijn vader heb ik deze website met veel plezier gemaakt.
-            Mijn hobbies zijn: voetballen, gamen en bier drinken. 
+            Ik ben erg blij met het resultaat!
             </p>
         </div>
     </div>
 </article>
 <br>
 <article>
-    <div class="row">
+    <div class="row" >
         <div class="three columns">
-            <img src="./images/team/sam.jpg" alt="Sam Wetzels" width="200px" height="200px">
+            <img class="team" src="./images/team/sam.jpg" alt="Sam Wetzels" width="200" height="200">
         </div>
         <div class="seven columns">
             <h5>Sam Wetzels</h5>
@@ -38,15 +37,11 @@
 <article>
     <div class="row">
         <div class="three columns">
-            <img src="./images/team/lieuwe.jpg" alt="Lieuwe van Weert" width="200px" height="200px">
+            <img class="team" src="./images/team/lieuwe.jpg" alt="Lieuwe van Weert" width="200" height="200">    
         </div>
         <div class="seven columns">
             <h5>Lieuwe van Weert</h5>
-            <p>Hallo! Mijn naam is Mees en ik woon in Amsterdam. 
-            Voor mijn studie KI moest ik een website bouwen en het eerste wat in mij opkwam was een webshop voor m'n moeder.
-            Ik wilde haar graag helpen bij het toegankelijker maken van haar producten.
-            Met behulp van mijn vader heb ik deze website met veel plezier gemaakt.
-            Mijn hobbies zijn: voetballen, gamen en bier drinken. 
+            <p>
             </p>
         </div>
     </div>
@@ -55,15 +50,15 @@
 <article>
     <div class="row">
         <div class="three columns">
-            <img src="./images/team/jop.jpg" alt="Jop Sutmuller" width="200px" height="200px">
+            <img class="team" src="./images/team/jop.jpg" alt="Jop Sutmuller" width="200" height="200">
         </div>
         <div class="seven columns">
             <h5>Jop Sutmuller</h5>
-            <p>Hallo! Mijn naam is Mees en ik woon in Amsterdam. 
-            Voor mijn studie KI moest ik een website bouwen en het eerste wat in mij opkwam was een webshop voor m'n moeder.
-            Ik wilde haar graag helpen bij het toegankelijker maken van haar producten.
-            Met behulp van mijn vader heb ik deze website met veel plezier gemaakt.
-            Mijn hobbies zijn: voetballen, gamen en bier drinken. 
+            <p>
+            Hey, ik ben Jop en ik studeer momenteel aan de UvA. 
+            Ik zit daarbij ook in het bestuur van de studievereniging <a href="https://www.svspectrum.nl/site/">Spectrum</a>. 
+            Net als mijn mede projectgenoten doe ik de bachelor Bèta-Gamma met de major Kunstmatige Intelligentie. 
+            Samen hebben we deze webshop gebouwd, hopelijk vindt u wat u zoekt.
             </p>
         </div>
     </div>
